ff1de0f879be47aa1265c8c12137423a737e8ba5
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-21 17:30:46 -0500

    Modified package.json file, added an exported file for each case of employee, and created a generalQuestions array.

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 //Sub Class of Employee
 const inquirer = require("inquirer");
-//fs needed? const fs = require("fs");
-//Need inquirer for each employee.js, engineer.js, intern.js etc
+const fs = require("fs");
+//Need inquirer for each employee.js, engineer.js, intern.js file etc
+// const asyncWrite = util.promisify(fs.writeFile)
 const Engineer = require ("./lib/Engineer");
 const intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+
 const employees = [];
 
-const generateHTML = (inquiryResponses) => {
-
+const generateHTML = (responses) => {
+''
 }
-inquirer
-  .prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name (First, Last)?',
-      },
-      {
-        type: 'input',
-        name: 'id',
-        message: 'What is your employee id number?',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your e-mail address?',
-      },
-      //Start of role questions:
-      {
-        type: 'checkbox',
-        name: 'role',
-        message: 'What is your role?',
-        choices: ['Manager','Intern','Engineer']
-      },
-      //Manager:
-      {
-        type: 'input',
-        name: 'officeNumber',
-        message: 'What is your working office number?',
-      },
-      //Engineer:
-      {
-        type: 'input',
-        name: 'gitHub',
-        message: 'What is your github username?',
-      },
-      //Intern:
-      {
-        type: 'input',
-        name: 'school',
-        message: 'What school do you go to?',
-      },
-    ]).then((inquiryResponses) => {
-        const filename = `${inquiryResponses.title}.html`; //'README.md' or '${inquiryResponses.title}.md' to name the top of the readmefile tab. Note:Putting it as readme.md WILL replace current readme file that I created.
-        return asyncWrite(filename, generateREADME(inquiryResponses) )}
-    ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
+generalQuestions = [
+  { 
+    type: 'input',
+    name: 'name',
+    message: 'What is your name (First, Last)?',
+  },{
+    type: 'input',
+    name: 'id',
+    message: 'What is your employee id number?',
+  },{
+    type: 'input',
+    name: 'email',
+    message: 'What is your e-mail address?',
+  },]
+// inquirer
+//   .prompt([
+//       generalQuestions[0],
+//       generalQuestions[1],
+//       generalQuestions[2],
+//       generalQuestions[3],
+//       //Manager:
+//       {
+//         type: 'input',
+//         name: 'officeNumber',
+//         message: 'What is your working office number?',
+//       },
+//       //Engineer:
+//       {
+//         type: 'input',
+//         name: 'gitHub',
+//         message: 'What is your github username?',
+//       },
+//       //Intern:
+//       {
+//         type: 'input',
+//         name: 'school',
+//         message: 'What school do you go to?',
+//       },
+    // ]).then((responses) => {
+    //     const filename = `${responses.title}.html`; //'README.md' or '${inquiryResponses.title}.md' to name the top of the readmefile tab. Note:Putting it as readme.md WILL replace current readme file that I created.
+    //     return asyncWrite(filename, generateHTML(responses) )}
+    // ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
 
