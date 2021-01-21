c4960706e4c04cff8bfbbc95b274d381a52c80ab
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-20 21:29:37 -0500

    Added general questions to index.js file.

--- index.js
+++ index.js
@@ -5,51 +5,24 @@ inquirer
   .prompt([
     {
         type: 'input',
-        name: 'title',
-        message: 'What would you like the title of this project to be called?',
+        name: 'name',
+        message: 'What is your name (First, Last)?',
       },
       {
         type: 'input',
-        name: 'description',
-        message: 'What is the description of your project?',
+        name: 'id',
+        message: 'What is your employee id number?',
       },
       {
         type: 'input',
-        name: 'installation',
-        message: 'What are the installation instructions you want to add for your project?',
-      },
-      {
-        type: 'input',
-        name: 'usageInfo',
-        message: 'What are the required usage information needed for this project?',
-      },
-      {
-        type: 'list',
-        message: 'What is the license for this project?',
-        name: 'license',
-        choices: ['MIT', 'Apache', 'Boost', 'BSD','Eclipse','None'],
-      },
-      {
-        type: 'input',
-        message: 'Any contributing guidelines you would like to add? If not applicable, type NA or just press enter',
-        name: 'contributingGuidelines',
-      },
-      {
-        type: 'input',
-        name: 'testInstructions',
-        message: 'What are the test instructions for this project?',
+        name: 'officeNumber',
+        message: 'What is your working office number?',
       },
       {
         type: 'input',
         name: 'email',
         message: 'What is your e-mail address?',
       },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'What is your github username?',
-      },
-
     ]).then((inquiryResponses) => {
         const filename = `${inquiryResponses.title}.`; //'README.md' or '${inquiryResponses.title}.md' to name the top of the readmefile tab. Note:Putting it as readme.md WILL replace current readme file that I created.
         return asyncWrite(filename, generateREADME(inquiryResponses) )}
