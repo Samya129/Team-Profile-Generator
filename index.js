bf2fa7d3a6acb7c12d6e421ef8b021f0696b2c87
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-22 01:28:51 -0500

    Organized code to correspond the same order throughout the entire js file. Also, broke the code because only subquestions are populating NOT general questions. For that reason, its commented out for now.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Sub Class of Employee
 const inquirer = require("inquirer");
 const fs = require("fs");
-//Need inquirer for each employee.js, engineer.js, intern.js file etc
 // const asyncWrite = util.promisify(fs.writeFile)
 const Engineer = require ("./lib/Engineer");
 const intern = require("./lib/Intern");
@@ -9,71 +8,54 @@ const Manager = require("./lib/Manager");
 
 const employees = [];
 
-const generateHTML = (responses) => {
-''
-}
 generalQuestions = [
   { 
-    type: 'input',
     name: 'name',
-    message: 'What is your name (First, Last)?',
-  },{
     type: 'input',
-    name: 'id',
-    message: 'What is your employee id number?',
-  },{
-    type: 'input',
+    message: "Please enter team member's name (First, Last)?",
+  },
+  {
     name: 'email',
-    message: 'What is your e-mail address?',
+    type: 'input',
+    message: "What is the team member's e-mail address?",
+  },
+  {
+    name: 'id',
+    type: 'input',
+    message: "What is the team member's id number?",
   },]
 // inquirer
 //   .prompt([
 //       generalQuestions[0],
 //       generalQuestions[1],
 //       generalQuestions[2],
-//       generalQuestions[3],
-//Manager:
-//       {
-//         type: 'input',
-//         name: 'officeNumber',
-//         message: 'What is your working office number?',
-//       },
-//       //Engineer:
-//       {
-//         type: 'input',
-//         name: 'gitHub',
-//         message: 'What is your github username?',
-//       },
-//       //Intern:
-//       {
-//         type: 'input',
-//         name: 'school',
-//         message: 'What school do you go to?',
-//       },
-
-    // ]).then((responses) => {
-    //     const filename = `${responses.title}.html`; //'README.md' or '${inquiryResponses.title}.md' to name the top of the readmefile tab. Note:Putting it as readme.md WILL replace current readme file that I created.
-    //     return asyncWrite(filename, generateHTML(responses) )}
-    // ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
+//      ])
+//     .then((response) => {
+//   //     console.log(response)
+//   // askQuestion(); 
+// },
+//         const filename = `${response.title}.html`; //'README.md' or '${inquiryResponse.title}.md' to name the top of the readmefile tab. Note:Putting it as readme.md WILL replace current readme file that I created.
+//         return asyncWrite(filename, generateHTML(respons) )}
+//     ) .then (()=>{ console.log("Success!")}).catch ((err)=>{console.log(err)}) ;
 
     const askQuestion = () =>{
         inquirer
         .prompt([
             {
-                type: "list",
-                message: "What would you like to do?",
-                choices: ["Add Manager","Add Intern","Add Engineer", "Build team"],
-                name: "userChoice",
+              name: "userChoice",
+              type: "list",
+              message: "What would you like to do?",
+              choices: ["Add Intern","Add Manager","Add Engineer", "Build team"],
     
             }
         ])
         .then(response => {
             switch (response.userChoice){
-                case "Add Manager": 
-                addManager();
-                break;
                 case "Add Intern": 
                 addIntern();
+                break;
+                case "Add Manager": 
+                addManager();
                 break;
                 case "Add Engineer": 
                 addEngineer();
@@ -85,63 +67,49 @@ generalQuestions = [
         })
     }
     askQuestion();
-    
-    const addManager = ()=>{
-        inquirer
-      .prompt([
-          generalQuestions[0],
-          generalQuestions[1],
-          generalQuestions[2],
-          //Manager:
-          {
-            type: 'input',
-            name: 'officeNumber',
-            message: 'What is your working office number?',
-          },
-        ])
-        .then(response =>{
-            console.log(response)
-            askQuestion();
-        })
-        
-    }
 
-    const addIntern = ()=>{
-      inquirer
-    .prompt([
-        generalQuestions[0],
-        generalQuestions[1],
-        generalQuestions[2],
-        //Intern:
-        {
-        type: 'input',
+//Intern Questions
+  const addIntern = ()=>{
+    inquirer
+  .prompt([
+      {
         name: 'school',
+        type: 'input',
         message: 'What school do you go to?',
-       },
-      ])
-      .then(response =>{
-          console.log(response)
-          askQuestion();
-      })
-      
-  }
+      },
+    ])
+    .then(response =>{
+        //console.log(response)
+        askQuestion();
+    }) 
+}
+//Manager Questions
+  const addManager = ()=>{
+    inquirer
+  .prompt([
+      {
+        name: 'officeNumber',
+        type: 'input',
+        message: 'What is your working office number?',
+      },
+    ])
+    .then(response =>{
+        //console.log(response)
+        askQuestion();
+    }) 
+}
+//Engineer Questions
   const addEngineer = ()=>{
     inquirer
   .prompt([
-      generalQuestions[0],
-      generalQuestions[1],
-      generalQuestions[2],
-      //Engineer:
       {
-        type: 'input',
         name: 'gitHub',
+        type: 'input',
         message: 'What is your github username?',
       },
     ])
     .then(response =>{
-        console.log(response)
+        //console.log(response)
         askQuestion();
     })
-    
 }
-    
