5b35817c7a276c8b87f7b270067458d6ec8bef97
Samya Fiki <crystalgrey@Samyas-MacBook-Pro.local>
2021-01-23 00:04:04 -0500

    Added another option for the intern test in case that one is more suitable.

--- Test/Intern.test.js
+++ Test/Intern.test.js
@@ -27,3 +27,67 @@ describe('Intern Sub Class', () => {
       });
     });
   });
+
+
+
+
+
+  describe("Intern", () => {
+    // TODO: Add a comment describing the purpose of the following test suite
+    describe("Initialization", () => {
+      //Testing if we can create a new child object
+      it("should create an object with a name, id, and email if provided valid arguments", () => {
+        const child = new Child("Sarah", 3);
+  
+        // TODO: Add a comment describing the purpose of the following statements
+        //checking to see if the parameters we passed through are attributes of the object
+        expect(child.name).toEqual("Sarah");
+        expect(child.age).toEqual(3);
+      });
+  
+      it("should throw an error if provided no arguments", () => {
+        // TODO: Add a comment describing the purpose of the following expression
+        //creating an empty child object
+        const cb = () => new Child();
+  
+        // TODO: Add a comment describing the purpose of the following statement
+        //checking to see if empty child object throws an error when passed through(no arguments)
+        expect(cb).toThrow();
+      });
+  
+      it("should throw an error if not provided an age", () => {
+        const cb = () => new Child("Sarah");
+  
+        // TODO: Add a comment describing the purpose of the following declaration
+        //has to be positive number or a number that is valid
+        const err = new Error("Expected parameter 'age' to be a non-negative number");
+  
+        // TODO: Add a comment describing the purpose of the following statement
+        //if only name is passed through and no age, throw the error this error specifically not in general
+        expect(cb).toThrowError(err);
+      });
+  
+      it("should throw an error if 'name' is not a string", () => {
+        const cb = () => new Child(3, 2);
+        const err = new Error("Expected parameter 'name' to be a non-empty string");
+  
+        expect(cb).toThrowError(err);
+      }); //if err error is thrown when nonstring name is passed through -> 3 is not a name
+  
+      it("should throw an error if 'age' is not a number", () => {
+        const cb = () => new Child("Sarah", "2");
+        const err = new Error("Expected parameter 'age' to be a non-negative number");
+  
+        expect(cb).toThrowError(err);
+      });//if err error is thrown when non-number is passed as a number
+  
+      it("should throw an error if 'age' is less than 0", () => {
+        const cb = () => new Child("Sarah", -1);
+        const err = new Error("Expected parameter 'age' to be a non-negative number");
+  
+        expect(cb).toThrowError(err);
+      });//if err error is thrown when the age passed is negative
+    });
+  });
+  
+  //unit test
